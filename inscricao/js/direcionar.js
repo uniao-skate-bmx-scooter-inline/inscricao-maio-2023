b5abeef3a373073f10bc4cbc09f049233b8fa2bd
uniao-skate-bmx-scooter-inline <132849539+uniao-skate-bmx-scooter-inline@users.noreply.github.com>
2023-05-13 12:53:38 -0300

    Delete inscricao directory

--- inscricao/js/direcionar.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('my-form').addEventListener('submit', (e) =>{
-      e.preventDefault()
-      fetch(e.target.action, {
-            method: 'POST',
-            body: new FormData(e.target)
-      }).then(() => {
-            window.location.href = 'https://uniao-skate-bmx-scooter-inline.github.io/inscricao-maio-2023/inscricao/'
-      })
-})
